e089ea8205e44667666451ac5d73df6419eddc72
Victor Gunnarsson <victor@educar.se>
2022-09-23 11:39:37 +0200

    inital commit

---
+++ lua/user/lsp/null-ls.lua
@@ -0,0 +1,35 @@
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
+end
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+
+-- https://github.com/prettier-solidity/prettier-plugin-solidity
+null_ls.setup {
+   on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+
+      -- format on save
+      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
+    end
+
+    if client.server_capabilities.documentRangeFormattingProvider then
+      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+    end
+  end, debug = false,
+  sources = {
+    formatting.prettier.with {
+      extra_filetypes = { "toml" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    },
+    formatting.black.with { extra_args = { "--fast" } },
+    formatting.stylua,
+    formatting.google_java_format,
+    diagnostics.flake8,
+  },
+}
